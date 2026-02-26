ccd19fe79deedc81601e183ad81214332e4849ec
VasiliPodoplielov <vasili.podoplelov@gmail.com>
2026-02-26 21:11:10 +0100

    add initial pages with basic structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { PlayersListComponent } from './components/players-list-component/players-list.component';
+import { LoginComponent } from './components/login-component/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'players', component: PlayersListComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '**', redirectTo: '' },
+];
